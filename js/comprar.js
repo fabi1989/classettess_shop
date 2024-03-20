0c270fb311b416ee2d62a2cd286837f5fa112aa8
fabian <zambranosalcedo@gmail.com>
2024-03-20 18:27:24 +0100

    primer_commit

--- js/comprar.js
+++ js/comprar.js
@@ -1,9 +1,9 @@
-const btn_Producto = document.getElementById('btn_agregarCarrito');
+/* const btn_Producto = document.getElementById('btn_agregarCarrito');
 
+  
 
 btn_Producto.addEventListener('click', ()=>{
-    //console.log()   
-    agregarAlcarrito();
+    
     //console.log("hola")
 
     const div = document.createElement('div');
@@ -15,7 +15,7 @@ btn_Producto.addEventListener('click', ()=>{
     precio.classList.add('precio');
 
     img.src = 'img/img1.jpg';
-    precio.textContent = '3,50 €';  
+    precio.textContent = '3,50 €'; 
 
     
     div.appendChild(img);
@@ -23,7 +23,7 @@ btn_Producto.addEventListener('click', ()=>{
     carrito.appendChild(div);
 
     
-    //agregarAlcarrito();
+    agregarAlcarrito();
 });
 
 
@@ -31,6 +31,59 @@ btn_Producto.addEventListener('click', ()=>{
 function agregarAlcarrito() {
     console.log("funcion agregar funciona!!")
     
+} */ 
+
+var carrito = document.getElementById("carrito");
+
+
+//crear obetos de los productos
+const producto1 = {
+    nombre: 'Calcetines',
+    precio: 3.50,
+    imagen: 'img1.jpg',
+    cantidad: 1,
+    total: 100,
+    id: 1
+ };
+
+ const producto2 = {
+    nombre: 'Calcetines',
+    precio: 3.50,
+    imagen: 'img2.jpg',
+    cantidad: 1,
+    total: 110,
+    id: 2
+ };
+
+ const producto3 = {
+    nombre: 'Calcetines',
+    precio: 3.50,
+    imagen: 'img2.jpg',
+    cantidad: 1,
+    total: 110,
+    id: 3
+ };
+//FIN OBJETOS DE LOS PRODUCTOS
+
+//ARRAY QUE CONTIENE LOS PRODUCTOS
+const listaProductos = [producto1, producto2, producto3];
+
+
+//FOR PARA RECORRER LISTA DE PRODUCTOS
+for (let i = 0; i < listaProductos.length; i++) {
+    console.log(listaProductos[i].id);
+    console.log(listaProductos[i].nombre);
+    console.log(listaProductos[i].precio);
 }
+
+var producto = listaProductos;
+
+    // Crea un elemento div para cada producto
+    var productoDiv = document.createElement("div");
+    productoDiv.className = "listaProducto";
+
+    
+
+    
 
 
