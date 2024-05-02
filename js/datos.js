c16c527d6beaa0718be32d63d53c656ca560c025
fabian <zambranosalcedo@gmail.com>
2024-05-02 17:37:59 +0200

    primero

---
+++ js/datos.js
@@ -0,0 +1,324 @@
+const calcetines = [
+    {
+        "id": "1",
+        "imagen": "img/img1.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "2",
+        "imagen": "img/img2.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "3",
+        "imagen": "img/img3.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "4",
+        "imagen": "img/img4.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "5",
+        "imagen": "img/img5.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "6",
+        "imagen": "img/img6.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "7",
+        "imagen": "img/img7.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "8",
+        "imagen": "img/img8.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "9",
+        "imagen": "img/img9.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "10",
+        "imagen": "img/img10.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "11",
+        "imagen": "img/img11.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "12",
+        "imagen": "img/img12.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "13",
+        "imagen": "img/img13.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "14",
+        "imagen": "img/img14.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "15",
+        "imagen": "img/img15.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "16",
+        "imagen": "img/img16.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "17",
+        "imagen": "img/img17.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "18",
+        "imagen": "img/img18.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "19",
+        "imagen": "img/img19.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "20",
+        "imagen": "img/img20.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "21",
+        "imagen": "img/img21.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "22",
+        "imagen": "img/img22.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "23",
+        "imagen": "img/img23.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "24",
+        "imagen": "img/img24.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "25",
+        "imagen": "img/img25.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "26",
+        "imagen": "img/img26.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "27",
+        "imagen": "img/img27.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "28",
+        "imagen": "img/img28.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "29",
+        "imagen": "img/img29.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "30",
+        "imagen": "img/img30.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "31",
+        "imagen": "img/img31.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "32",
+        "imagen": "img/img32.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "33",
+        "imagen": "img/img33.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "34",
+        "imagen": "img/img34.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "35",
+        "imagen": "img/img35.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "36",
+        "imagen": "img/img36.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "37",
+        "imagen": "img/img37.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "38",
+        "imagen": "img/img38.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "39",
+        "imagen": "img/img39.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "40",
+        "imagen": "img/img40.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "41",
+        "imagen": "img/img41.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "42",
+        "imagen": "img/img42.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "43",
+        "imagen": "img/img43.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "44",
+        "imagen": "img/img44.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "45",
+        "imagen": "img/img45.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "46",
+        "imagen": "img/img46.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "47",
+        "imagen": "img/img47.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "48",
+        "imagen": "img/img48.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "49",
+        "imagen": "img/img49.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    },
+    {
+        "id": "50",
+        "imagen": "img/img50.jpg",
+        "titulo": "¡Añádelo a tu colección de moda ahora mismo!",
+        "precio": "3.50€",
+    }
+    
+]
+
+
+const cardContainer = document.querySelector('.container-products');
+const nuevaFuncion = ()=>{
+    calcetines.forEach((calcetin)=>{
+    const nuevaCard = document.createElement('div');
+    nuevaCard.classList.add('product');
+
+        nuevaCard.innerHTML= `
+            <img src="${calcetin.imagen}" class="product__img" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${calcetin.titulo}</h5>
+                <p class="card-text">${calcetin.precio}</p>
+                <a href="#" class="btn_agregarCarrito">Comprar</a>
+            </div>
+        `
+        cardContainer.appendChild(nuevaCard);
+    })
+    
+}
+nuevaFuncion();
